766d218c85b22c63427b68d048ed763e1480f6a3
Thibaut LE LEVIER <thibaut@lelevier.fr>
2015-06-03 13:35:53 +0200

    handle redistogo urls

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,9 @@
 if ENV["REDIS_URL"]
   uri = URI.parse(ENV["REDIS_URL"])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+elsif ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else  
   $redis = Redis.new
 end
