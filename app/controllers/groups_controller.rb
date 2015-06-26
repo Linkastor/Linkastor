6fabc1dc7f739a30e8069100fbb3b8644d8f69aa
Vincent Daubry <vdaubry@gmail.com>
2015-06-26 16:00:54 +0200

    display pending invitations in group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -37,6 +37,7 @@ class GroupsController < ApplicationController
     @links = Link.where(:group_id => @group.id).order(created_at: :desc).paginate(:page => params[:page])
     @days = @links.group_by { |t| t.created_at.beginning_of_day }
     @invalid_emails = params[:invalid_emails]
+    @pending_invites = @group.invites.pending
   end
 
   private
