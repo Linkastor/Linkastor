080c01eafe89a3c2d32531929bb1a68f0afede00
Vincent Daubry <vdaubry@gmail.com>
2015-06-27 10:22:22 +0200

    feature flip custom source for admin users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626070023) do
+ActiveRecord::Schema.define(version: 20150627074541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,9 +91,10 @@ ActiveRecord::Schema.define(version: 20150626070023) do
     t.string   "email"
     t.string   "name"
     t.string   "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "nickname"
+    t.boolean  "admin",      default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
