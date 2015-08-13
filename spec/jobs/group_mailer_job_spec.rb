eb34607eba76d5aadfd1d01c762abaac5913e05d
Vincent Daubry <vdaubry@gmail.com>
2015-08-13 18:50:42 +0200

    fix specs

--- spec/jobs/group_mailer_job_spec.rb
+++ spec/jobs/group_mailer_job_spec.rb
@@ -22,13 +22,6 @@ describe GroupMailerJob do
       GroupMailerJob.new.perform
     end
     
-    it "imports all sources" do
-      CustomSourcesUser.create(user: @user, custom_source: twitter)
-      FactoryGirl.create(:link, custom_source: twitter)
-      CustomSources::Twitter.any_instance.expects(:import).once
-      GroupMailerJob.new.perform
-    end
-    
     it "marks sent links as posted" do
       link = FactoryGirl.create(:link, group: @group, posted: false)
       GroupMailerJob.new.perform
@@ -47,18 +40,6 @@ describe GroupMailerJob do
       GroupMailerJob.new.perform
       link.reload.posted_at.should_not be_nil
       link2.reload.posted_at.should == Date.parse("2015-08-11")
-    end
-
-    context "fail to send email to a user" do
-      it "keeps sending mail to next users" do
-        user2 = FactoryGirl.create(:user, admin: false)
-        user2.groups << @group
-        FactoryGirl.create(:link, group: @group, posted: false)
-        CustomSourcesUser.create(user: @user, custom_source: twitter)
-        CustomSources::Twitter.any_instance.stubs(:import).raises(StandardError)
-        DigestMailer.expects(:send_digest).once
-        GroupMailerJob.new.perform
-      end
     end
   end
 end
