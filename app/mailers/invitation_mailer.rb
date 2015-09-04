d959e0883ead7083ea7c2abe2d0703c92106d785
Vincent Daubry <vdaubry@gmail.com>
2015-09-04 09:17:01 +0200

    fix mail

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -4,6 +4,6 @@ class InvitationMailer < ApplicationMailer
     @referrer = invite.referrer
     @group = invite.group
     @join_url = invite_url(invite.code)
-    mail(to: @invite.email, subject: "#{@referrer.name} invites you to join #{@group.name} on Linkastor")
+    mail(to: @invite.email, subject: "#{@referrer.name} invites you to join #{@group.name} on Linkastor", skip_premailer: INLINE_MAIL_CSS)
   end
 end
