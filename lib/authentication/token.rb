315faf82e43f21cb76cd5ef2496b651c907181d6
Vincent Daubry <vdaubry@gmail.com>
2015-09-18 21:52:14 +0200

    fix typo

--- lib/authentication/token.rb
+++ lib/authentication/token.rb
@@ -20,12 +20,14 @@ module Authentication
     end
     
     private
+      attr_reader :user
+    
       def self.key(token:)
         "authentication_"+token
       end
+
   end
 
-  private
-  attr_reader :user
+
 
 end
